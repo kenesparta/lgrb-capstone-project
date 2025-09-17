7cfffd6c773325d720244c9114346269d11ede23
Ken Esparta <kenesparta@pm.me>
2025-09-17 08:37:33 -0500

    feat: setting the project to deploy to micro:bit

--- listener/src/main.rs
+++ listener/src/main.rs
@@ -1,1 +1,5 @@
-fn main() {}
+use leptos::prelude::*;
+
+fn main() {
+    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+}
--- nrf-embed/src/main.rs
+++ nrf-embed/src/main.rs
@@ -1,19 +1,79 @@
 #![no_std]
 #![no_main]
 
-use cortex_m::asm::nop;
-use cortex_m_rt::entry;
-use cortex_m_semihosting::hprintln;
+use defmt_rtt as _;
 use panic_halt as _;
-use rtt_target::{rprintln, rtt_init_print};
+
+use cortex_m_rt::entry;
+use embedded_hal::delay::DelayNs;
+use microbit::{board::Board, display::blocking::Display, hal::Timer};
 
 #[entry]
 fn main() -> ! {
-    let mut x: usize = 0;
-    loop {
-        x += 1;
-        for _ in 0..10000000 {
-            nop()
+    if let Some(board) = Board::take() {
+        let mut timer = Timer::new(board.TIMER0);
+        let mut display = Display::new(board.display_pins);
+
+        #[allow(non_snake_case)]
+        let letter_I = [
+            [0, 1, 1, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 1, 1, 1, 0],
+        ];
+
+        let heart = [
+            [0, 1, 0, 1, 0],
+            [1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 0],
+        ];
+
+        #[allow(non_snake_case)]
+        let letter_R = [
+            [0, 1, 1, 1, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0],
+        ];
+
+        let letter_u = [
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [0, 1, 1, 1, 0],
+        ];
+
+        let letter_s = [
+            [0, 0, 1, 1, 1],
+            [0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0],
+            [1, 1, 1, 0, 0],
+        ];
+
+        let letter_t = [
+            [1, 1, 1, 1, 1],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+        ];
+        loop {
+            display.show(&mut timer, letter_I, 1000);
+            display.show(&mut timer, heart, 1000);
+            display.show(&mut timer, letter_R, 1000);
+            display.show(&mut timer, letter_u, 1000);
+            display.show(&mut timer, letter_s, 1000);
+            display.show(&mut timer, letter_t, 1000);
+            display.clear();
+            timer.delay_ms(250_u32);
         }
     }
+
+    panic!("End");
 }
