aecc24ee26a1c5bf849d0491b40fe06f4b8c0b5e
Ken Esparta <kenesparta@pm.me>
2025-09-24 12:20:27 -0500

    feat: using microbit-bsp

--- nrf-embed/memory.x
+++ nrf-embed/memory.x
@@ -1,6 +1,13 @@
-/* Nordic Semiconductor nRF52833 */
 MEMORY
 {
   FLASH : ORIGIN = 0x00000000, LENGTH = 512K
   RAM : ORIGIN = 0x20000000, LENGTH = 128K
+}
+
+/* Add defmt section for proper logging support */
+SECTIONS
+{
+  .defmt : {
+    *(.defmt .defmt.*)
+  } > FLASH
 }
--- nrf-embed/src/main.rs
+++ nrf-embed/src/main.rs
@@ -1,70 +1,37 @@
 #![no_std]
 #![no_main]
 
-mod button;
-mod led;
-
-use panic_rtt_target as _;
-
-use button::ButtonDirection;
 use embassy_executor::Spawner;
-use embassy_nrf::{
-    Peri,
-    gpio::{AnyPin, Input, Level, Output, OutputDrive, Pull},
-};
-use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
-use embassy_time::Timer;
-use futures::{FutureExt, select_biased};
-use led::LedRow;
-use rtt_target::rtt_init_print;
-
-static CHANNEL: Channel<CriticalSectionRawMutex, ButtonDirection, 1> = Channel::new();
+use embassy_futures::select::{select, Either};
+use embassy_time::Duration;
+use microbit_bsp::*;
+use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
-    rtt_init_print!();
-    let p = embassy_nrf::init(Default::default());
+async fn main(_spawner: Spawner) {
+    let board = Microbit::default();
 
-    spawner
-        .spawn(button_task(p.P0_14.into(), ButtonDirection::Left))
-        .unwrap();
-    spawner
-        .spawn(button_task(p.P0_23.into(), ButtonDirection::Right))
-        .unwrap();
+    let mut display = board.display;
+    let mut btn_a = board.btn_a;
+    let mut btn_b = board.btn_b;
 
-    let _row1 = led_pin(p.P0_21.into());
-    let col = [
-        led_pin(p.P0_28.into()),
-        led_pin(p.P0_11.into()),
-        led_pin(p.P0_31.into()),
-        led_pin(p.P1_05.into()),
-        led_pin(p.P0_30.into()),
-    ];
-
-    // LED task:
-    let mut blinker = LedRow::new(col);
+    display.set_brightness(display::Brightness::MAX);
+    display.scroll("Hello, World!").await;
+    defmt::info!("Application started, press buttons!");
     loop {
-        blinker.toggle();
-        select_biased! {
-            direction = CHANNEL.receive().fuse() => {
-                blinker.shift(direction);
+        match select(btn_a.wait_for_low(), btn_b.wait_for_low()).await {
+            Either::First(_) => {
+                defmt::info!("A pressed");
+                display
+                    .display(display::fonts::ARROW_LEFT, Duration::from_secs(1))
+                    .await;
             }
-            _ = Timer::after_millis(500).fuse() => {}
+            Either::Second(_) => {
+                defmt::info!("B pressed");
+                display
+                    .display(display::fonts::ARROW_RIGHT, Duration::from_secs(1))
+                    .await;
+            }
         }
-    }
-}
-
-fn led_pin(pin: Peri<'static, AnyPin>) -> Output<'static> {
-    Output::new(pin, Level::High, OutputDrive::Standard)
-}
-
-#[embassy_executor::task(pool_size = 2)]
-async fn button_task(pin: Peri<'static, AnyPin>, direction: ButtonDirection) {
-    let mut input = Input::new(pin, Pull::None);
-    loop {
-        input.wait_for_low().await;
-        CHANNEL.send(direction).await;
-        Timer::after_millis(100).await;
-        input.wait_for_high().await;
     }
 }
